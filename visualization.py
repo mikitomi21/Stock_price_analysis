12ab86f7ece86bea956c3cfab86887457e5e3d9c
mikitomi21 <kubastach2001@gmail.com>
2023-03-06 18:12:17 +0100

    Fix bugs with interpreter and load data with pandas

--- visualization.py
+++ visualization.py
@@ -1,11 +1,16 @@
+import pandas as pd
 from ecofunction import macd, ema, signal
 from trader import Trader, Share
 
-Player = Trader(1000)
+START_MONEY_ACCOUNT = 1000
+
+Player = Trader(START_MONEY_ACCOUNT)
+intel_value = pd.read_csv("intc_us_d.csv").iloc[30:]
 
 while True:
     if (input("Click q to exit. Click enter to continue: ") == 'q'):
         break
+    #TODO 
 
 print("End of the visualization. Your money account is ", Player.get_money_account())
 
